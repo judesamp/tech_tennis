b5a77792167ff410b24532de2a2294f2400fcd6e
Jeremy Samples <judesamp@gmail.com>
2012-08-16 12:24:32 -0700

    Changed how spec gets to domain files. Needs work probably.

--- spec/domain/entities/game_spec.rb
+++ spec/domain/entities/game_spec.rb
@@ -1,7 +1,1 @@
 require '../spec_helper'
-require '../../../domain/entities/game'
-
-# this MIGHT work
-# Dir.glob(File.expand_path("./domain") +"/**/*.rb").each do |file|
-#   require file
-# end
--- spec/domain/spec_helper.rb
+++ spec/domain/spec_helper.rb
@@ -1,2 +1,10 @@
 require 'spec'
 require 'rack/test'
+
+#load all your stuff either explicitly:
+require '../../../domain/entities/game'
+
+# this MIGHT work for the whole deal
+# Dir.glob(File.expand_path("./domain") +"/**/*.rb").each do |file|
+#   require file
+# end
