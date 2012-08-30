eb98aeac455184e3077f2dd68c7606b0f9fe3334
Jeremy Samples <judesamp@gmail.com>
2012-08-30 07:49:23 -0700

    testing interaction between Ruby and web implementation

--- domain/entities/game.rb
+++ domain/entities/game.rb
@@ -4,7 +4,7 @@ module Domain
     def greeter
       "<h3>Do you want to sign in or play as a guest?</h3>
       <ul id = 'greeting'>
-      <li><button type='input'>Sign in</button> </li>
+      <li><button onclick='window.open('http://www.espn.com')' type='input'>Sign in</button> </li>
        <li><button type='input'>Play as Guest</button></li>
       </ul>"
     end
