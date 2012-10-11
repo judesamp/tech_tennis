9d8fb8cdea36155b713366a7a9df42a761e94cee
Jeremy Samples <judesamp@gmail.com>
2012-10-11 12:02:14 -0700

    Added step file.

--- features/get_profile.feature
+++
@@ -1,9 +0,0 @@
-Feature: after greeting, play as guest or create profile
-	As an interested party
-	I want to play some Tech Tennis
-	So that I can have fun and learn a little about HTML, CSS, and Ruby
-	Scenario:
-	Given I have arrived at Tech Tennis site
-	When my page loads
-	Then I should see a welcome message
-	And I should have the option to play as a guest or create a profile
--- features/play_game_feature.rb
+++
@@ -1,9 +0,0 @@
-Feature: match begins
-	As a tennis player (and student of the web)
-	I want to defeat my opponent (and do a little studying at the same time)
-	So that I can move on to the next round and learn a little HTML and CSS
-	Scenario: Opponent serves
-	Given I am ready to play and started tech_tennis
-	When my match starts
-	Then I should see an alert
-	And I should see a question.
