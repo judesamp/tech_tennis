f2dde6619dbfcaa7a0a5b89160c8c050e451aeda
Jeremy Samples <judesamp@gmail.com>
2013-02-19 19:34:57 -0500

    adjusting representer

--- implementation/api/v1/representers/game_representer.rb
+++ implementation/api/v1/representers/game_representer.rb
@@ -4,11 +4,14 @@ require 'roar/representer/feature/hypermedia'
 module Implementation
   module API
     
+    
+    module Bowl
+    
     # class Play
     #           attr_accessor :game_id, :current_role, :user_score, :user_game, :user_set, :opponent_score, :opponent_game, :opponent_set, :completed_in, :leftover_time, :last_result, :question_id, :question_text, :answer_option_a, :answer_option_b, :answer_option_c, :answer_option_d, :current_quiz_type, :times_asked, :game_context, :user_answer
     #         end
     
-    module PlayRepresenter
+    module DataRepresenter
       include Roar::Representer::JSON
       include Roar::Representer::Feature::Hypermedia
 
@@ -33,6 +36,14 @@ module Implementation
       property :times_asked
       property :game_context
       property :user_answer    
+    end
+    
+    module HolderRepresenter
+      include Roar::Representer::JSON
+      include Roar::Representer::Feature::Hypermedia
+      
+      collection :data, :class => Data, :extend => DataRepresenter, 
+      :embedded => true
     end
   end
 end
